640810b0d5a2cc767a0526abf9cfc73385538581
thanawateiei <122959522+thanawateiei@users.noreply.github.com>
2023-01-18 13:02:08 +0700

    create project

---
+++ ProjectVanStation/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectVanStation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
